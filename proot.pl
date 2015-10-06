9a6d258c8f72e2b413ec1abc1571869ed5572100
Heng Li <lh3@me.com>
2015-10-05 21:34:47 -0400

    launch script

---
+++ proot.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+die("Usage: proot.pl <rootfs> <cmd> [arguments]\n") if @ARGV < 2;
+
+# look for 'proot'
+my $proot = &gwhich('proot') || die("ERROR: failed to find the 'proot' executable\n");
+
+# skip some system mount points
+my @skip = ('/', '/usr', '/lib', '/lib64', '/usr/lib', '/usr/lib64');
+my %skip;
+$skip{$_} = 1 for (@skip);
+
+# get the list of non-system mount points
+my ($fh, @mnt);
+open($fh, "df -P |") || die;
+while (<$fh>) {
+	next if $. == 1;
+	my @t = split;
+	my $last = $t[@t-1];
+	push(@mnt, "-b $last") unless defined($skip{$last});
+}
+close($fh);
+
+# run
+my $rootfs = shift(@ARGV);
+exec $proot, "-r $rootfs", @mnt, @ARGV;
+
+# misc
+
+sub which {
+	my $file = shift;
+	my $path = (@_)? shift : $ENV{PATH};
+	return if (!defined($path));
+	foreach my $x (split(":", $path)) {
+		$x =~ s/\/$//;
+		return "$x/$file" if (-x "$x/$file") && (-f "$x/$file");
+	}
+	return;
+}
+
+sub gwhich {
+    my $progname = shift;
+    my $addtional_path = shift if (@_);
+    my $dirname = &dirname($0);
+    my $tmp;
+
+    chomp($dirname);
+    if ($progname =~ /^\// && (-x $progname) && (-f $progname)) {
+        return $progname;
+    } elsif (defined($addtional_path) && ($tmp = &which($progname, $addtional_path))) {
+        return $tmp;
+    } elsif (defined($dirname) && (-x "$dirname/$progname") && (-f "$dirname/$progname")) {
+        return "$dirname/$progname";
+    } elsif ((-x "./$progname") && (-f "./$progname")) {
+        return "./$progname";
+    } elsif (($tmp = &which($progname))) {
+        return $tmp;
+    } else {
+        return;
+    }
+}
+
+sub dirname {
+	my $prog = shift;
+	return '.' unless ($prog =~ /\//);
+	$prog =~ s/\/[^\s\/]+$//g;
+	return $prog;
+}
